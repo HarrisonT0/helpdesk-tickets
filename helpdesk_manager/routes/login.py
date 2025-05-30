70e2bf5e9c37012ca59b5073f42fc9fd58616981
HarrisonT0 <harrison.t_2023_lon@outlook.com>
2025-05-30 12:50:17 +0100

    feat: added login auth flow

--- helpdesk_manager/routes/login.py
+++ helpdesk_manager/routes/login.py
@@ -1,6 +1,23 @@
-from flask import current_app as app
+from flask import current_app as app, request, session, render_template, redirect
+from werkzeug.security import check_password_hash
+from ..models.user import User
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return "Login"
+    error = None
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+
+        if user is None:
+            error = "Invalid email."
+        elif not check_password_hash(user.password_hash, password):
+            error = "Invalid password."
+        else:
+            session["user_id"] = user.id
+            return redirect("/")
+
+    return render_template("login.html", error=error)
