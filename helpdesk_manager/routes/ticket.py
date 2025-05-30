72eeeaa8c851c86274d20840384201f53f1acdc8
HarrisonT0 <harrison.t_2023_lon@outlook.com>
2025-05-30 14:11:19 +0100

    feat: improved validation of ticket creation

--- helpdesk_manager/routes/ticket.py
+++ helpdesk_manager/routes/ticket.py
@@ -1,21 +1,40 @@
-from flask import current_app as app, request, session, render_template, redirect
+from flask import (
+    current_app as app,
+    request,
+    session,
+    render_template,
+    redirect,
+    url_for,
+)
 from helpdesk_manager.models.ticket import Ticket
 from ..database import db
 
 
 @app.route("/ticket/new", methods=["GET", "POST"])
 def new_ticket():
+    if "user_id" not in session:
+        return redirect(url_for("login"))
+
+    error = None
+
     if request.method == "POST":
         # Get form inputs
         title = request.form.get("title")
         content = request.form.get("content")
         user_id = session["user_id"]
 
-        # Create ticket
-        ticket = Ticket(title=title, content=content, author_id=user_id)
-        db.session.add(ticket)
-        db.session.commit()
+        # Validation
+        if not title:
+            error = "Title is required."
+        elif not content:
+            error = "Content is required."
 
-        return redirect("/")
+        # If all checks pass
+        else:
+            ticket = Ticket(title=title, content=content, author_id=user_id)
+            db.session.add(ticket)
+            db.session.commit()
 
-    return render_template("tickets/new.html")
+            return redirect("/")
+
+    return render_template("tickets/new.html", error=error)
