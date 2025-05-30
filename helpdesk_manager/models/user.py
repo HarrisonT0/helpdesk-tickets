8a52e196d19910d182540860be76c4c87ec6d4ed
HarrisonT0 <harrison.t_2023_lon@outlook.com>
2025-05-30 11:10:38 +0100

    feat: added initial user model

---
+++ helpdesk_manager/models/user.py
@@ -0,0 +1,16 @@
+from ..database import db
+import uuid
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, DateTime
+from datetime import datetime
+
+
+class User(db.Model):
+    __tablename__ = "user"
+
+    id: Mapped[str] = mapped_column(
+        String(36), primary_key=True, default=lambda: str(uuid.uuid4()), unique=True
+    )
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password_hash = Mapped[str] = mapped_column(String(128), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now())
