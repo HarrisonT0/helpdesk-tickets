1a34f7354418f264884a527c8e2b4de3e179724f
HarrisonT0 <harrison.t_2023_lon@outlook.com>
2025-06-01 11:54:38 +0100

    chore: added login attempt with wrong password test

--- tests/test_auth_flow.py
+++ tests/test_auth_flow.py
@@ -2,7 +2,6 @@ import requests
 import uuid
 
 ROUTE = "http://localhost:5000"
-MESSAGE = "View tickets here"
 
 
 def test_register():
@@ -24,7 +23,7 @@ def test_register():
         home_response = session.get(f"{ROUTE}")
 
         # Confirm page successfully loaded and auth wall was not hit
-        assert MESSAGE in home_response.text
+        assert "View tickets here" in home_response.text
 
 
 def test_login():
@@ -45,7 +44,7 @@ def test_login():
         home_response = session.get(f"{ROUTE}/")
 
         # Confirm page successfully loaded and auth wall was not hit
-        assert MESSAGE in home_response.text
+        assert "View tickets here" in home_response.text
 
 
 def test_logout():
@@ -72,4 +71,33 @@ def test_logout():
         home_response = session.get(f"{ROUTE}")
 
         # Confirm auth wall was hit and page did not load
-        assert MESSAGE not in home_response.text
+        assert "View tickets here" not in home_response.text
+
+
+def test_login_wrong_password():
+    email = f"test-{uuid.uuid4()}@company.com"
+
+    # Register a new account with random username
+    register_response = requests.post(
+        f"{ROUTE}/register",
+        data={
+            "email": email,
+            "password": "Password123!",
+            "confirm_password": "Password123!",
+        },
+    )
+
+    # Confirm register request responded
+    assert register_response
+
+    # Attempt to log in with incorrect password
+    login_response = requests.post(
+        f"{ROUTE}/login",
+        data={
+            "email": email,
+            "password": "incorrect",
+        },
+    )
+
+    # Confirm log in did not succeed
+    assert "Invalid password" in login_response.text
