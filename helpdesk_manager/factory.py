288f0c8c3092e55d9b95d2dcf19f30da56c77ff0
HarrisonT0 <harrison.t_2023_lon@outlook.com>
2026-01-21 16:40:19 +0000

    chore: removed unnecessary database model imports in factory

--- helpdesk_manager/factory.py
+++ helpdesk_manager/factory.py
@@ -1,28 +1,23 @@
 from flask import Flask
 from .database import db
 from .utils.seed_database import seed_database
-from werkzeug.security import generate_password_hash
 
 
 def create_app():
     app = Flask(__name__)
 
-    # Database
+    # Database config
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///helpdesk.db"
     app.config["SECRET_KEY"] = "SECRET_CHANGE_ME"
     db.init_app(app)
 
     # App context
     with app.app_context():
-        # Register table models
-        from helpdesk_manager.models.user import User
-        from helpdesk_manager.models.ticket import Ticket
-        from helpdesk_manager.models.comment import Comment
-
+        # Register Database
         db.create_all()
         seed_database()
 
-        # Register blueprints
+        # Register Blueprints
         from helpdesk_manager.routes.tickets import tickets_bp
         from helpdesk_manager.routes.auth import auth_bp
         from helpdesk_manager.routes.comments import comments_bp
