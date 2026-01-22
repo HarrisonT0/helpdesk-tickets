23a07476da29fdbafde085735b9379a2ff4c54fc
HarrisonT0 <harrison.t_2023_lon@outlook.com>
2026-01-22 09:07:42 +0000

    feat(security): added validity of credentials check in require auth route decorator

--- helpdesk_manager/utils/require_auth.py
+++ helpdesk_manager/utils/require_auth.py
@@ -1,13 +1,26 @@
 from functools import wraps
 from flask import session, redirect, flash
 
+from helpdesk_manager.models.user import User
+
 
 def require_auth(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if not session.get("user_id"):
+        user_id = session.get("user_id")
+
+        # No credentials presented
+        if not user_id:
             flash("You must be logged in to access this page.", "error")
             return redirect("/login")
+
+        # Credentials invalid/expired
+        user = User.query.get(user_id)
+        if not user:
+            session.clear()
+            flash("Your session expired. Please log in again.", "error")
+            return redirect("/login")
+
         return f(*args, **kwargs)
 
     return decorated_function
